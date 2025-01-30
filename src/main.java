11f6d6c918af23a2c3dedaf9145f046ce87e8089
Rodrigo Silveira <rodrigo.garra77@hotmail.com>
2025-01-30 11:51:23 -0300

    end

--- src/main.java
+++ src/main.java
@@ -4,22 +4,29 @@ import  java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        List<String> listaM = new ArrayList<>();
+        List<String> listaF = new ArrayList<>();
 
         Scanner s = new Scanner(System.in);
         System.out.println("Qual o seu nome?");
         String name = s.next();
 
+
         s = new Scanner(System.in);
-        System.out.println("Qual seu sexo?");
+        System.out.println("Qual seu sexo? M para Masculino e F para Feminino");
         String sexo = s.next();
 
-        List<String> lista = new ArrayList<>();
-        lista.add(name);
-        lista.add(sexo);
+        if (sexo.equalsIgnoreCase("M")) {
+            listaM.add(name);
+        } else if (sexo.equalsIgnoreCase("F")) {
+            listaF.add(name);
+        }
 
         System.out.println(name);
         System.out.println(sexo);
-        System.out.println(lista);
+        System.out.println(listaM);
+        System.out.println(listaF);
+
     }
 }
 
