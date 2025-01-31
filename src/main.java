e3c4ae5ad1fbab6013dc51ce919f32368b17211d
Rodrigo Desk <rodrigo.garra77@hotmail.com>
2025-01-31 20:25:30 -0300

    ebac2

--- src/main.java
+++ src/main.java
@@ -7,26 +7,40 @@ public class main {
         List<String> listaM = new ArrayList<>();
         List<String> listaF = new ArrayList<>();
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Qual o seu nome?");
-        String name = s.next();
+        String continuar = "S";
+        while(continuar.equalsIgnoreCase("S")){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Qual o seu nome?");
+            String name = s.next();
+            String[] partes = name.split(" ");
 
 
-        s = new Scanner(System.in);
-        System.out.println("Qual seu sexo? M para Masculino e F para Feminino");
-        String sexo = s.next();
 
-        if (sexo.equalsIgnoreCase("M")) {
-            listaM.add(name);
-        } else if (sexo.equalsIgnoreCase("F")) {
-            listaF.add(name);
+
+                s = new Scanner(System.in);
+                System.out.println("Qual seu sexo? M para Masculino e F para Feminino");
+                String sexo = s.next();
+
+                if (sexo.equalsIgnoreCase("M")) {
+                    listaM.add(name);
+                } else if (sexo.equalsIgnoreCase("F")) {
+                    listaF.add(name);
+                }
+
+                s = new Scanner(System.in);
+                System.out.println("Continuar?  S/N");
+                continuar = s.next();
+
+
+
+
+
+           // System.out.println(name);
+           // System.out.println(sexo);
+                System.out.println("Masculino" + listaM);
+           // else
+                System.out.println("Feminino:" + listaF);
         }
-
-        System.out.println(name);
-        System.out.println(sexo);
-        System.out.println(listaM);
-        System.out.println(listaF);
-
     }
 }
 
